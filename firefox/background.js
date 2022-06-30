e32ed2da52abc910ac96a0e4c91ace074a261501
VadimasKo <kozemiakinas123@gmail.com>
2022-06-30 13:19:01 +0300

    fix listeners

--- firefox/background.js
+++ firefox/background.js
@@ -15,32 +15,23 @@ setBlockList()
 
 //initialise Request listenere
 const onBeforeRequest = browser.webRequest.onBeforeRequest
+const blockCallBack = () => {  return { cancel: true } }
 
-let onBeforeListener
 const setOnBeforeListener = () => {
-  onBeforeListener = onBeforeRequest.addListener(
-    () => {  return { cancel: true } },
+  onBeforeRequest.addListener(
+    blockCallBack,
     { urls: blockList?.length ? blockList : [''] },
     ["blocking"]
   )
 }
 
-//listen for config changes and updateQ
-// WHY ARE YOU NOT RUNNING!!! 
-// https://www.youtube.com/watch?v=dv0k5J9YDuM
-// browser.storage.onChanged.addListener(changes => {
-//   console(changes, 'onChanged')
-//   blockList = changes.blockList.newValue
-//   onBeforeRequest.removeListener(onBeforeListener)
-//   setOnBeforeListener()
-// })
-
+//listen for config changes and update
 const bc = new BroadcastChannel('taskEnforcer');
 
 bc.onmessage = async event => {
-  console.log(event.data)
+  console.log('update', event.data)
   const buffer = await browser.storage.sync.get('blockList')
   blockList = buffer.blockList
-  onBeforeRequest.removeListener(onBeforeListener)
+  onBeforeRequest.removeListener(blockCallBack)
   setOnBeforeListener()
 }
