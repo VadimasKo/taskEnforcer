f2fa94e60e13c1a8e3c01c51849ab57ae4241c72
VadimasKo <kozemiakinas123@gmail.com>
2022-07-05 15:31:39 +0300

    fix sort bug

--- firefox/common/index.js
+++ firefox/common/index.js
@@ -17,8 +17,8 @@ const convertDeadline = ({ deadline }) => {
 // Time functions
 const isFirstTimeSmaller = (time1, time2) => {
   const lessHours = time1[0] < time2[0]
-  const sameHours = time1[0] == time2[0]
-  const lessMinutes = time1[1] <= time2[1]
+  const sameHours = time1[0] === time2[0]
+  const lessMinutes = time1[1] < time2[1]
 
   return lessHours || (sameHours && lessMinutes)
 }
@@ -35,14 +35,14 @@ const sortTasks = (taskList) => {
   taskList.sort((task1, task2) => {
     const time1 = convertDeadline(task1)
     const time2 = convertDeadline(task2)
-  
+
     const isTime1Before = isBeforeCurrentTime(time1)
     const isTime2Before = isBeforeCurrentTime(time2)
 
     if (!isTime1Before && isTime2Before) return -1 //return task1
     if (isTime1Before && !isTime2Before) return 1 //return task2
 
-    return isFirstTimeSmaller(task1, task2) ? -1 : 1
+    return isFirstTimeSmaller(time1, time2) ? -1 : 1
   })
 
   return taskList
